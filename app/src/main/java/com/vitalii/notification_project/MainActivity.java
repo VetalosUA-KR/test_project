1dfbc6a0594f46b4e31c3e733805fdf63296fdc1
Vitalii Voitenko <vetal925@gmail.com>
2020-03-07 13:35:50 +0100

    test

--- app/src/main/java/com/vitalii/notification_project/MainActivity.java
+++ app/src/main/java/com/vitalii/notification_project/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int tttt = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
